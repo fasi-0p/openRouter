c989b0c98cb80018bf3014a61093d5e36e0d1714
Fasi Owaiz Ahmed <fasiowaizahmed1234@gmail.com>
2026-03-07 18:48:54 +0530

    Signup page frontend done

--- apps/dashboard-frontend/src/App.tsx
+++ apps/dashboard-frontend/src/App.tsx
@@ -8,21 +8,31 @@ import {Dashboard} from "./pages/Dashboard"
 import {Credits} from "./pages/Credits"
 import {ApiKeys} from "./pages/ApiKeys"
 import {Landing} from "./pages/Landing"
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import {ElysiaClientContextProvider} from "./providers/Eden"
 
+// const client = treaty<App>(process.env.BACKEND_URL || 'https://localhost:3000')
+const client = treaty<App>( 'https://localhost:3000') //todo : line 14 and 15
+
+const queryClient = new QueryClient()
 
 export function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/credits" element={<Credits />} />
-        <Route path="/api-keys" element={<ApiKeys />} />
-      </Routes>
-    </BrowserRouter>
+    <ElysiaClientContextProvider value={client}>
+      <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/signin" element={<Signin />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/credits" element={<Credits />} />
+              <Route path="/api-keys" element={<ApiKeys />} />
+            </Routes>
+          </BrowserRouter>
+        </QueryClientProvider>
+    </ElysiaClientContextProvider>
   );
 }
 
--- apps/primary-backend/src/index.ts
+++ apps/primary-backend/src/index.ts
@@ -1,3 +1,5 @@
 import {app} from "./app"
+// import {cors} from "@elysiajs/cors"
+// app.use(cors()).listen(3000)
 
 app.listen(3000)
